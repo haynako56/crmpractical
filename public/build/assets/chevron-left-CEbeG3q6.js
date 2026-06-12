150b849b654fbe7da18c30470e47fe0d356ca354
Richard Bahillo <richard.bahillo05@gmail.com>
2026-06-12 18:37:41 +0800

    npm build files

---
+++ public/build/assets/chevron-left-CEbeG3q6.js
@@ -0,0 +1,1 @@
+import{D as e}from"./app-BXfCT1P-.js";var t=e(`ChevronLeft`,[[`path`,{d:`m15 18-6-6 6-6`,key:`1wnfg3`}]]);export{t};
--- public/build/assets/chevron-left-C_9fTd8g.js
+++
@@ -1,1 +0,0 @@
-import{D as e}from"./app-M0UUSCa0.js";var t=e(`ChevronLeft`,[[`path`,{d:`m15 18-6-6 6-6`,key:`1wnfg3`}]]);export{t};
