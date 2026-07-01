057defda8c2704069f4f22c08a003a502f52d2a7
Richard Bahillo <richard.bahillo05@gmail.com>
2026-07-01 15:35:14 +0800

    cron schedule

--- routes/console.php
+++ routes/console.php
@@ -8,6 +8,6 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 
-Schedule::command('enquiries:sync-cf7')->hourly();
+Schedule::command('enquiries:sync-cf7')->everyFiveMinutes();
 Schedule::command('assignments:send-reminders')->dailyAt('10:00');
 Schedule::command('enquiries:send-no-response-alerts')->dailyAt('09:00');
